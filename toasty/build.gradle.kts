56a0710a6cea280e8c5e0fa7e7389fda49c202dc
GijareShreeyash <shreeyash.g@reapmind.com>
2024-06-05 12:05:28 +0530

    Initial Commit 4

--- toasty/build.gradle.kts
+++ toasty/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("release") {
+            create<MavenPublication>("maven") {
                 from(components["release"])
 
                 groupId = "com.github.GijareShreeyash"
